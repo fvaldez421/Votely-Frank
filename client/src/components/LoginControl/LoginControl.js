9ab0f5eed1c2a65d8bbe741b154b14eef669f4de
Frank Valdez <fvaldez421@gmail.com>
2018-04-30 17:54:49 -0700

    added franks GAPI clientId

--- client/src/components/LoginControl/LoginControl.js
+++ client/src/components/LoginControl/LoginControl.js
@@ -5,25 +5,27 @@ import { GoogleAPI, GoogleLogin, GoogleLogout } from "react-google-oauth";
 
 // Login Control Component
 const LoginControl = (props) => (
-    
-    
-        <div className="sizeAuto">
-        <GoogleAPI 
-        clientId="470848001164-2l4g92q85okvv703tf7ptnllvtci31km.apps.googleusercontent.com"
-        fetch_basic_profile="true" >
-          { props.isLoggedIn ?
-              <GoogleLogout 
-                onLogoutSuccess={props.handleLogoutClick} 
-            	>
-              </GoogleLogout>
-            : 
-              <GoogleLogin 
-  	          	onLoginSuccess={props.handleLoginClick} 
-            	/> 
-          }
-        </GoogleAPI>
-        </div>
-      );
+  
+  <div className="sizeAuto">
+    <GoogleAPI 
+      clientId="470848001164-2l4g92q85okvv703tf7ptnllvtci31km.apps.googleusercontent.com"
+      fetch_basic_profile="true" >
+        { props.isLoggedIn ?
+          <GoogleLogout 
+            onLogoutSuccess={props.handleLogoutClick} 
+            />
+          : 
+          <GoogleLogin 
+            onLoginSuccess={props.handleLoginClick} 
+            /> 
+        }
+    </GoogleAPI>
+  </div>
+);
   
   
   export default LoginControl;
+
+
+  // Daniels clientId: "470848001164-2l4g92q85okvv703tf7ptnllvtci31km.apps.googleusercontent.com"
+  // Franks clientId: "338508164539-lg1qobf7ue0nco24i25ua331f7qrmg25.apps.googleusercontent.com"
